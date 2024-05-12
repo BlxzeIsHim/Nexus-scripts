23a20396b936f705ca32075bca4e421adcb08f3e
BlxzeIsHim <primesto000@gmail.com>
2024-05-12 16:15:27 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,21 +1,25 @@
-document.addEventListener("DOMContentLoaded", function() {
-    var input = document.getElementById("searchInput");
+<script>
+    document.addEventListener("DOMContentLoaded", function() {
+        var input = document.getElementById("searchInput");
 
-    input.addEventListener("keypress", function(event) {
-        if (event.key === "Enter") {
-            search();
-        }
+        input.addEventListener("keypress", function(event) {
+            if (event.key === "Enter") {
+                search();
+            }
+        });
     });
-});
 
-function search() {
-    var searchTerm = document.getElementById("searchInput").value.toLowerCase();
-    if (searchTerm === "blade ball") {
-        window.location.href = "bladeball.html";
-    } else if (searchTerm === "counter blox") {
-        window.location.href = "counterblox.html";
-    } else {
-        // Implement other search functionality or display a message for no results found
-        alert("No results found for: " + searchTerm);
+    function search() {
+        var searchTerm = document.getElementById("searchInput").value.toLowerCase();
+        if (searchTerm === "blade ball") {
+            window.location.href = "bladeball.html";
+        } else if (searchTerm === "counter blox") {
+            window.location.href = "counterblox.html";
+        } else if (searchTerm === "doors") {
+            window.location.href = "doors.html";
+        } else {
+            // Implement other search functionality or display a message for no results found
+            alert("No results found for: " + searchTerm);
+        }
     }
-}
+</script>
