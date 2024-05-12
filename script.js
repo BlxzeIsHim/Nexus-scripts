31ce9710e2aa86c60e3dd019b00a0dff767f74bd
BlxzeIsHim <primesto000@gmail.com>
2024-05-12 14:03:27 -0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,19 @@
+document.addEventListener("DOMContentLoaded", function() {
+    var input = document.getElementById("searchInput");
+
+    input.addEventListener("keypress", function(event) {
+        if (event.key === "Enter") {
+            search();
+        }
+    });
+});
+
+function search() {
+    var searchTerm = document.getElementById("searchInput").value.toLowerCase();
+    if (searchTerm === "blade ball") {
+        window.location.href = "bladeball.html";
+    } else {
+        // Implement other search functionality or display a message for no results found
+        alert("No results found for: " + searchTerm);
+    }
+}
