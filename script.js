60bd03e670ba6ce23efb47f6fc544dd754696f64
BlxzeIsHim <primesto000@gmail.com>
2024-05-12 15:31:22 -0700

    Update script.js

--- script.js
+++ script.js
@@ -12,6 +12,8 @@ function search() {
     var searchTerm = document.getElementById("searchInput").value.toLowerCase();
     if (searchTerm === "blade ball") {
         window.location.href = "bladeball.html";
+    } else if (searchTerm === "counter blox") {
+        window.location.href = "counterblox.html";
     } else {
         // Implement other search functionality or display a message for no results found
         alert("No results found for: " + searchTerm);
