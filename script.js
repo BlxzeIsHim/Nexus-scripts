312980cd87bf2cdcf83d1018dcf001345de615d8
BlxzeIsHim <primesto000@gmail.com>
2024-05-13 13:11:35 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,3 @@
-document.addEventListener("DOMContentLoaded", function() {
-    var input = document.getElementById("searchInput");
-
-    input.addEventListener("keypress", function(event) {
-        if (event.key === "Enter") {
-            search();
-        }
-    });
-});
-
 function search() {
     var searchTerm = document.getElementById("searchInput").value.toLowerCase();
     if (searchTerm === "blade ball") {
@@ -16,8 +6,10 @@ function search() {
         window.location.href = "counterblox.html";
     } else if (searchTerm === "doors") {
         window.location.href = "doors.html";
+    } else if (searchTerm === "blox fruits") {
+        window.location.href = "bloxfruits.html";
     } else {
-        // Implement other search functionality or display a message for no results found
-        alert("No results found for: " + searchTerm);
+        // Redirect to the scriptnotfound.html for unknown games
+        window.location.href = "scriptnotfound.html";
     }
 }
